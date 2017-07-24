b6e4e20a8b45ce7b7c8634b07cd091e225dcbca5
guillermo <gallego.bonet@gmail.com>
2017-07-24 21:45:48 +0200

    improving numerically rotation so that R2AA does not throw error

--- matlab/coordinate_transforms/q2R.m
+++ matlab/coordinate_transforms/q2R.m
@@ -1,19 +1,18 @@
 function R = q2R( q )
 % function R = q2R( q )
 % Convert quaternion (q) to 3x3 rotation matrix (R)
-% 
+%
 % Input:
 %     q: [qw, qx, qy, qz]*[1 i j k]';
-% 
+%
 % Ouput:
 %     R: a 3x3 Rotation Matrix
-%     
+%
 % Written by Garrick Orchard July 2017
 % Based on:
 % http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToMatrix/
 
 R = zeros(3);
-
 
 R(1,1) = 1 - 2*(q(3)^2 + q(4)^2);
 R(1,2) = 2*(q(2)*q(3) - q(4)*q(1));
@@ -26,3 +25,27 @@ R(2,3) = 2*(q(3)*q(4) - q(2)*q(1));
 R(3,1) = 2*(q(2)*q(4) - q(3)*q(1));
 R(3,2) = 2*(q(3)*q(4) + q(2)*q(1));
 R(3,3) = 1 - 2*(q(2)^2 + q(3)^2);
+
+% Numerically improve result by projecting on the space of rotation matrices
+[u,~,v] = svd(R);
+R = u * diag([1,1,det(u*v.')]) * v.'; % Closest rot matrix
+
+
+% % Method using q*q.'
+% % qq = q(:)*q(:).';
+% % Rp  = eye(3) - 2* [qq(3,3)+qq(4,4),   qq(4,1)-qq(2,3), -(qq(3,1)+qq(2,4)); ...
+% %                  -(qq(2,3)+qq(4,1)),  qq(4,4)+qq(2,2),   qq(2,1)-qq(3,4); ...
+% %                    qq(3,1)-qq(2,4), -(qq(2,1)+qq(3,4)),  qq(2,2)+qq(3,3)];
+
+% % Method using determinants. As good as previous ones
+% R(1,1) = det([q(1)+q(3), q(2)+q(4); -q(2)+q(4), q(1)-q(3)]);
+% R(1,2) = 2 * det([q(2),q(4);  q(1),q(3)]);
+% R(1,3) = 2 * det([q(3),q(2); -q(4),q(1)]);
+% 
+% R(2,1) = 2 * det([q(2),q(4); -q(1),q(3)]);
+% R(2,2) = det([q(1)+q(2), q(3)+q(4); -q(3)+q(4), q(1)-q(2)]);
+% R(2,3) = 2 * det([q(4),q(1); q(2),q(3)]);
+% 
+% R(3,1) = 2 * det([q(4),q(1);  q(3),q(2)]);
+% R(3,2) = 2 * det([q(4),q(1); -q(2),q(3)]);
+% R(3,3) = det([q(1)+q(3), q(2)-q(4); q(2)+q(4), q(1)-q(3)]);
