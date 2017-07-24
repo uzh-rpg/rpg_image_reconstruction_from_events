5b875bfce3ebd6abb35999c02849263875470a0f
guillermo <gallego.bonet@gmail.com>
2017-07-24 19:47:42 +0200

    Improved rotation parameter conversion functions

--- matlab/coordinate_transforms/AA2R.m
+++ matlab/coordinate_transforms/AA2R.m
@@ -1,23 +1,34 @@
 function R = AA2R(AA)
 % function R = AA2R(AA)
 % Convert Axis Angle (AA) to Rotation Matrix (R)
-% 
+%
 %  Input
 %     AA: 1x4 axis angle rotation.
-% 
+%
 % Ouput:
 %     R: a 3x3 rotation matrix
-%     
+%
 % Written by Garrick Orchard July 2017
 % Based on:
 % http://mathworld.wolfram.com/RodriguesRotationFormula.html
 
-AA(1:3) = AA(1:3)./norm(AA(1:3)); %ensure a unit length vector for the axis
+assert(numel(AA)==4,'Input must be 1x4 or 4x1');
 
-omega = [ 0,    -AA(3), AA(2);...
-        AA(3),     0, -AA(1);...
-       -AA(2), AA(1), 0];
-   
+% Axis
+norm_ax = norm(AA(1:3));
+if (norm_ax < 1e-6)
+    R = eye(3);
+    return
+end
+ax = AA(1:3) ./ norm_ax; % Unit norm, avoid division by zero
+
+% Cross-product matrix
+omega = [   0, -ax(3),  ax(2);
+        ax(3),      0, -ax(1);
+       -ax(2),  ax(1),  0];
+
+% Rotation angle
 theta = AA(4);
 
+% Rotation matrix, using Rodrigues formula
 R = eye(3) + omega*sin(theta) + omega*omega*(1-cos(theta));
