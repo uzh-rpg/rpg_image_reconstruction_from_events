21ae804afb99d857675cab080284eaef6f1f0311
guillermo <gallego.bonet@gmail.com>
2017-07-25 10:05:50 +0200

    Using 2 function handles instead of a boolean per toolbox. The function can also work with just 3 input arguments

--- matlab/rotationAt.m
+++ matlab/rotationAt.m
@@ -1,4 +1,4 @@
-function rot_interp = rotationAt(t_ctrl, rotmats_ctrl, t_query, use_robotics_system_toolbox, use_VR_toolbox)
+function rot_interp = rotationAt(t_ctrl, rotmats_ctrl, t_query, f_r2a, f_a2r)
 % rotationAt Function for linear interpolation of rotations
 % Interpolate the orientation (rotation) at a given time.
 %
@@ -6,15 +6,42 @@ function rot_interp = rotationAt(t_ctrl, rotmats_ctrl, t_query, use_robotics_sys
 % -t_ctrl: timestamps of discrete set of orientations ("control poses")
 % -rotmats_ctrl: discrete set of rotation matrices
 % -t_query: time of the requested rotation matrix
-% -use_robotics_system_toolbox: true or false, depending on toolbox availability
-% -use_VR_toolbox: true or false, depending on toolbox availability
+% -f_r2a: (Optional) handle to function that converts rotation to axis-angle
+% -f_a2r: (Optional) handle to function that converts axis-angle to rotation
 %
 % Output:
 % -rot_interp: interpolated rotation matrix
 %
 % I/O in the style of interp1
 
+% Check function handles
+if (nargin < 5) || ~isa(f_r2a,'function_handle') || ~isa(f_a2r,'function_handle')
+    
+    % If no conversion functions are provided, specify them
+    
+    if license('test', 'Robotics_System_Toolbox')
+        % Requires Robotic System toolbox
+        f_r2a = @rotm2axang;
+        f_a2r = @axang2rotm;
+        
+    else
+        % Use VR toolbox if Robotics System is not available
+        toolboxes_info = ver;
+        if any(strcmp('Simulink 3D Animation', {toolboxes_info.Name}))
+            % Requires VR toolbox
+            f_r2a = @vrrotmat2vec;
+            f_a2r = @vrrotvec2mat;
+            
+        else
+            % Use custom conversion functions, which should be in the path
+            f_r2a = @R2AA;
+            f_a2r = @AA2R;
+        end
+    end
+end
 
+
+% Rotation interpolation
 if (t_query < t_ctrl(1) || t_query > t_ctrl(end) )
     rot_interp = nan * rotmats_ctrl(1,:);
     
@@ -23,31 +50,15 @@ elseif (t_query == t_ctrl(end))
     
 else
     idx_0 = find(t_query > t_ctrl,1,'last');
-    % two rotations and their times
+    % Two rotations and their times
     t_0 = t_ctrl(idx_0);
     t_1 = t_ctrl(idx_0+1);
     rot_0 = rotmats_ctrl(:,:,idx_0);
     rot_1 = rotmats_ctrl(:,:,idx_0+1);
-    % interpolation parameter in [0,1]
+    % Interpolation parameter in [0,1]
     dt = (t_query - t_0) / (t_1 - t_0);
     % Linear interpolation, Lie group formulation
-    rot_increm = rot_0.'*rot_1;
-    if use_robotics_system_toolbox
-        % Requires Robotic System toolbox
-        axang_increm = rotm2axang(rot_increm);
-        axang_increm(4) = axang_increm(4)*dt;
-        rot_interp = rot_0 * axang2rotm( axang_increm );
-        
-    elseif use_VR_toolbox
-        % Requires VR toolbox
-        axang_increm = vrrotmat2vec(rot_increm);
-        axang_increm(4) = axang_increm(4)*dt;
-        rot_interp = rot_0 * vrrotvec2mat( axang_increm );
-        
-    else
-        % Removes dependency on the Robotic System toolbox
-        axang_increm = R2AA(rot_increm);
-        axang_increm(4) = axang_increm(4)*dt;
-        rot_interp = rot_0 * AA2R( axang_increm );
-    end
+    axang_increm = f_r2a( rot_0.'*rot_1 );
+    axang_increm(4) = axang_increm(4)*dt;
+    rot_interp = rot_0 * f_a2r( axang_increm );
 end
